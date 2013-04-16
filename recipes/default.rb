74d7f91cfc5e712b3c862f076d730010c045121a
Julia Astakhova <jastakhova@griddynamics.com>
2013-04-16 15:42:45 -0700

    fix for sources.list in ubuntu maverick

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,3 +5,17 @@ directory "#{Chef::Config[:node_path]}" do
   action :create
   recursive true
 end
+
+if (platform?("ubuntu") && node['platform_version'].to_i == 10)
+  execute "apt-get update" do
+    ignore_failure true
+    action :nothing
+  end
+
+  template "/etc/apt/sources.list" do
+    source "ubuntu10.sources.list.erb"
+    notifies :run, resources(:execute => "apt-get update"), :immediately
+    mode 0644
+  end
+end
+
